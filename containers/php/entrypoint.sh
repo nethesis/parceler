7282a634440480ea69ae4f19e0e000e3ecb02762
Tommaso Bailetti <tommaso.bailetti@nethesis.it>
2024-05-03 14:26:15 +0200

    build: moved around container init

--- containers/php/entrypoint.sh
+++ containers/php/entrypoint.sh
@@ -11,10 +11,10 @@ if [ "$1" = 'php-fpm' ]; then
         composer install
     else
         php artisan optimize
-        chown -R www-data:www-data storage
     fi
     php artisan migrate --force
-elif [ "$1" = 'crond' ] || [ "$3" = 'queue:work' ]; then
+    chown -R www-data:www-data storage
+elif [ "$1" = 'crond' ] || [ "$3" = 'queue:work' ] || [ "$3" = 'queue:listen' ]; then
     wait-for "${PHP_HOST:?Missing PHP_HOST}:${PHP_PORT:?Missing PHP_PORT}" -t 60
 fi
 
