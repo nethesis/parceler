d18f7f4a6512b458207eb6940616514955563981
Tommaso Bailetti <tommaso.bailetti@nethesis.it>
2024-05-08 10:28:34 +0200

    fix: waiting for app to setup completely

--- containers/php/entrypoint-dev.sh
+++ containers/php/entrypoint-dev.sh
@@ -10,8 +10,10 @@ if [ "$1" = "php-fpm" ]; then
     composer install
     php artisan migrate --force
 elif [ "$1" = "scheduler" ]; then
+    wait-for "${PHP_HOST:?Missing PHP_HOST}:${PHP_PORT:?Missing PHP_PORT}" -t 60
     set -- crond -f
 elif [ "$1" = "worker" ]; then
+    wait-for "${PHP_HOST:?Missing PHP_HOST}:${PHP_PORT:?Missing PHP_PORT}" -t 60
     set -- php artisan queue:work --tries=3 --timeout=1800
 fi
 
--- containers/php/entrypoint.sh
+++ containers/php/entrypoint.sh
@@ -13,9 +13,11 @@ if [ "$1" = "php-fpm" ]; then
     php artisan migrate --force
 elif [ "$1" = "scheduler" ]; then
     # remove scheduler command, replace the $@
+    wait-for "${PHP_HOST:?Missing PHP_HOST}:${PHP_PORT:?Missing PHP_PORT}" -t 60
     set -- crond -f
 elif [ "$1" = "worker" ]; then
     # remove worker command, replace the $@
+    wait-for "${PHP_HOST:?Missing PHP_HOST}:${PHP_PORT:?Missing PHP_PORT}" -t 60
     set -- su -s /bin/sh -c "php artisan queue:work --tries=3 --timeout=1800" www-data
 fi
 
