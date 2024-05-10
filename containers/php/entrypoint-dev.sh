86ea21ee3c517add19b5848aabf848ba29b950a0
Tommaso Bailetti <tommaso.bailetti@nethesis.it>
2024-05-10 12:01:32 +0200

    fix: using queue:listen on dev environments

--- containers/php/entrypoint-dev.sh
+++ containers/php/entrypoint-dev.sh
@@ -14,7 +14,7 @@ elif [ "$1" = "scheduler" ]; then
     set -- crond -f
 elif [ "$1" = "worker" ]; then
     wait-for "${PHP_HOST:?Missing PHP_HOST}:${PHP_PORT:?Missing PHP_PORT}" -t 60
-    set -- php artisan queue:work --tries=3 --timeout=1800
+    set -- php artisan queue:listen --tries=3 --timeout=1800
 fi
 
 exec "$@"
