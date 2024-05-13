1615a87aefce35d17cdb8442aa62cdcb9a08f126
Tommaso Bailetti <tommaso.bailetti@nethesis.it>
2024-05-13 16:14:41 +0200

    fix(scheduler): misspell costed me another release

--- routes/console.php
+++ routes/console.php
@@ -2,11 +2,10 @@
 
 use App\Jobs\SyncRepository;
 use App\Models\Repository;
-use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Schedule;
 
 Schedule::call(function () {
     foreach (Repository::cursor() as $repository) {
-        Artisan::call(SyncRepository::class, [$repository->name]);
+        SyncRepository::dispatch($repository);
     }
-})->daily();
+})->name('Sync Repositories')->daily();
