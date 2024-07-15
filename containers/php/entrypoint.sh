68ce4abc66fdcc2dd763abe47779fa9180fa878e
Tommaso Bailetti <tommaso.bailetti@nethesis.it>
2024-07-15 17:20:48 +0200

    refactor: improved dockerfile for php

--- containers/php/entrypoint.sh
+++ containers/php/entrypoint.sh
@@ -6,21 +6,26 @@ if [ -f '.env' ]; then
     . .env
 fi
 
-php artisan optimize
+if [ "$APP_ENV" != "local" ]; then
+    php artisan optimize
+fi
 
 if [ "$1" = "php-fpm" ]; then
-    # migrate database
+    composer install
     php artisan migrate --force
-    # TODO when moving to a DB, remove the following line
-    chown -R www-data:www-data storage
+    if [ "$(id -u)" = '0' ]; then
+        chown -R www-data:www-data storage
+    fi
 elif [ "$1" = "scheduler" ]; then
-    # remove scheduler command, replace the $@
     wait-for "${PHP_HOST:?Missing PHP_HOST}:${PHP_PORT:?Missing PHP_PORT}" -t 60
-    set -- crond -f
+    set -- crond -f -d 6
 elif [ "$1" = "worker" ]; then
-    # remove worker command, replace the $@
     wait-for "${PHP_HOST:?Missing PHP_HOST}:${PHP_PORT:?Missing PHP_PORT}" -t 60
-    set -- su -s /bin/sh -c "php artisan queue:work --tries=3 --timeout=1800" www-data
+    if [ "$(id -u)" = '0' ]; then
+        set -- su -s /bin/sh -c "php artisan queue:work --tries=3 --timeout=1800" www-data
+    else
+        set -- php artisan queue:work --tries=3 --timeout=1800
+    fi
 fi
 
 exec "$@"
