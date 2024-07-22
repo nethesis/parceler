a9e9ce68b179b04f8b7f48a8e98bb1ebc20d6e6f
Tommaso Bailetti <tommaso.bailetti@nethesis.it>
2024-07-22 11:35:44 +0200

    fix: fixing issue with composer in prod environment

--- containers/php/entrypoint.sh
+++ containers/php/entrypoint.sh
@@ -11,21 +11,19 @@ if [ "$APP_ENV" != "local" ]; then
 fi
 
 if [ "$1" = "php-fpm" ]; then
-    composer install
+    if [ "$APP_ENV" = "local" ]; then
+        composer install
+    fi
     php artisan migrate --force
     if [ "$(id -u)" = '0' ]; then
         chown -R www-data:www-data storage
     fi
 elif [ "$1" = "scheduler" ]; then
     wait-for "${PHP_HOST:?Missing PHP_HOST}:${PHP_PORT:?Missing PHP_PORT}" -t 60
-    set -- crond -f -d 6
+    set -- crond -f -d
 elif [ "$1" = "worker" ]; then
     wait-for "${PHP_HOST:?Missing PHP_HOST}:${PHP_PORT:?Missing PHP_PORT}" -t 60
-    if [ "$(id -u)" = '0' ]; then
-        set -- su -s /bin/sh -c "php artisan queue:work --tries=3 --timeout=1800" www-data
-    else
-        set -- php artisan queue:work --tries=3 --timeout=1800
-    fi
+    set -- su -s /bin/sh -c "php artisan queue:work --tries=3 --timeout=1800" www-data
 fi
 
 exec "$@"
