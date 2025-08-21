511e0d8970e0a1a58be1fe9b8fd8ff31bc88efe9
Tommaso Bailetti <tommaso.bailetti@nethesis.it>
2025-08-21 12:32:53 +0200

    feat: added redis as cache

--- containers/php/entrypoint.sh
+++ containers/php/entrypoint.sh
@@ -2,38 +2,38 @@
 
 set -e
 
-if [ -f '.env' ]; then
-    . .env
-fi
+wait_for_services() {
+    wait-for "${PHP_HOST:?Missing PHP_HOST}:${PHP_PORT:?Missing PHP_PORT}" -t 60
+}
 
-if [ "$1" = "php-fpm" ]; then
+optimize() {
     if [ "$APP_ENV" != "local" ]; then
         php artisan optimize
-    else
+    fi
+}
+
+if [ "$1" = "php-fpm" ]; then
+    optimize
+    if [ "$APP_ENV" = "local" ]; then
         composer install
     fi
     php artisan migrate --force
     if [ "$(id -u)" = '0' ]; then
         chown -R www-data:www-data storage
     fi
+    exec "$@"
 elif [ "$1" = "scheduler" ]; then
-    wait-for "${PHP_HOST:?Missing PHP_HOST}:${PHP_PORT:?Missing PHP_PORT}" -t 60
-    if [ "$APP_ENV" != "local" ]; then
-        php artisan optimize
-    fi
-    set -- crond -f -d 8
+    wait_for_services
+    optimize
+    exec su -s /bin/sh -c "php artisan schedule:work --quiet" www-data
 elif [ "$1" = "worker" ]; then
-    wait-for "${PHP_HOST:?Missing PHP_HOST}:${PHP_PORT:?Missing PHP_PORT}" -t 60
-    if [ "$APP_ENV" != "local" ]; then
-        php artisan optimize
-    fi
-    set -- su -s /bin/sh -c "php artisan queue:work --tries=3 --timeout=1800" www-data
+    wait_for_services
+    optimize
+    exec su -s /bin/sh -c "php artisan queue:work --tries=3 --timeout=1800" www-data
 elif [ "$1" = "nightwatch" ]; then
-    wait-for "${PHP_HOST:?Missing PHP_HOST}:${PHP_PORT:?Missing PHP_PORT}" -t 60
-    if [ "$APP_ENV" != "local" ]; then
-        php artisan optimize
-    fi
-    set -- su -s /bin/sh -c "php artisan nightwatch:agent --listen-on 0.0.0.0:2407" www-data
+    wait_for_services
+    optimize
+    exec su -s /bin/sh -c "php artisan nightwatch:agent --listen-on 0.0.0.0:2407" www-data
+else
+    exec "$@"
 fi
-
-exec "$@"
