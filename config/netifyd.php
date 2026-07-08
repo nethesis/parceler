af29bfe660e256fb3b1a845eb40712fd4c90900e
Tommaso Bailetti <tommaso.bailetti@nethesis.it>
2026-07-08 09:06:46 +0200

    fix: time limiting the rate limiter

--- config/netifyd.php
+++ config/netifyd.php
@@ -9,4 +9,6 @@ return [
     'api-key' => env('NETIFYD_API_KEY'),
     'license-suffix' => env('NETIFYD_LICENSE_SUFFIX'),
     'rate-limit' => env('NETIFYD_RATE_LIMIT', 180),
+    'rate-limit-start-hour' => env('NETIFYD_RATE_LIMIT_START_HOUR', 1),
+    'rate-limit-end-hour' => env('NETIFYD_RATE_LIMIT_END_HOUR', 6),
 ];
